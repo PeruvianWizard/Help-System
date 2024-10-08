c6cc070418efea6b97f8f41993228d16d0885d01
Shawn Hudman <shawnhudman@me.com>
2024-10-07 17:18:17 -0700

    Updated Admin log in controller and screen.

--- CSE360Project/src/HelpSystem/FirstLogInWindowController.java
+++ CSE360Project/src/HelpSystem/FirstLogInWindowController.java
@@ -7,8 +7,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -17,7 +19,9 @@ public class FirstLogInWindowController {
 	private Scene theScene;
 	private Parent theRoot;
 	
-
+    @FXML
+    private Label passNotSameLabel;
+	
     @FXML
     private PasswordField passInput1;
 
@@ -27,9 +31,53 @@ public class FirstLogInWindowController {
     @FXML
     private TextField usernameInput;
 
+    String username;
+    char[] password;
+    
+    // blocks log in button until pw is the same.
+    boolean passSame = false;
+    
+    @FXML
+    void checkPasswords(KeyEvent event) {
+    	String p1 = passInput1.getText();
+    	String p2 = passInput2.getText();
+    	
+    	if(p1.equals(p2)) {
+    		passNotSameLabel.setVisible(false);
+    		
+    		passSame = true;
+    	} else {
+    		passNotSameLabel.setVisible(true);
+    		
+    		passSame = false;
+    	}
+    }
+    
     @FXML
     void SetUser(MouseEvent event) {
+    	if(passSame == false) { 
+    		// throw some error
     		
+    		return;
+    	} else {
+    		username = usernameInput.getText();						
+        	password = passInput1.getText().toCharArray();
+    	}
     }
+    
+    @FXML
+    public void updateAndSwitchToRegisterWindow(ActionEvent event) throws IOException {
+    	if(passSame == false) {
+    		passNotSameLabel.setVisible(true);
+    		
+    		return; 
+    	} else {
+    		Parent theRoot = FXMLLoader.load(getClass().getResource("HelpSystemLogInWindow.fxml"));
+    		theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+    		theScene = new Scene(theRoot);
+    		theStage.setScene(theScene);
+    		theStage.show();
+    	}
+	}
 
 }
