a8ba871d87d56f1dad4e5d2b671b9b2b50fb35c8
alfie <alfie@PC_del_Wizard.3521.dhcp.asu.edu>
2024-10-22 13:29:03 -0700

    Quick Fixes To LogInAsRoleWindow and LogInAsRoleWindowController\
    A little bit of tomfoolery

--- CSE360Project/src/HelpSystem/LogInAsRoleWindowController.java
+++ CSE360Project/src/HelpSystem/LogInAsRoleWindowController.java
@@ -1,48 +1,109 @@
 package HelpSystem;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class LogInAsRoleWindowController {
 	private Stage theStage;
 	private Scene theScene;
 	private Parent theRoot;
 	
-	String username;
 	
+    @FXML
+    private ImageView explosion;
+
+    @FXML
+    private Label explosionLabel;
+
+    @FXML
+    private Label warningLabel;
 	
+    PauseTransition pause = new PauseTransition(Duration.seconds(1.5));
+    PauseTransition pause2 = new PauseTransition(Duration.seconds(1.5));
+    PauseTransition pause3 = new PauseTransition(Duration.seconds(1.5));
+    
 	@FXML
-    void SwitchToAdminWindow(ActionEvent event) throws IOException {
-		Parent theRoot = FXMLLoader.load(getClass().getResource("AdminWindow.fxml"));
-		theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		theScene = new Scene(theRoot);
-		theStage.setScene(theScene);
-		theStage.show();
+    void SwitchToAdminWindow(ActionEvent event) throws IOException, SQLException {
+		if(HelpSystem.userDatabaseHelper.checkAuth(HelpSystem.getUsername()).contains("admin")) {
+			Parent theRoot = FXMLLoader.load(getClass().getResource("AdminWindow.fxml"));
+			theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			theScene = new Scene(theRoot);
+			theStage.setScene(theScene);
+			theStage.show();
+		}
+		else {
+			warningLabel.setVisible(false);
+			explosionLabel.setVisible(true);
+			explosion.setVisible(true);
+			
+			pause.setOnFinished(e-> explosion.setVisible(false));
+			pause2.setOnFinished(e-> explosionLabel.setVisible(false));
+			pause3.setOnFinished(e-> warningLabel.setVisible(true));
+			
+			pause.play();
+			pause2.play();
+			pause3.play();
+			
+		}
     }
 
     @FXML
-    void SwitchToInstructionalWindow(ActionEvent event) throws IOException {
-
-    	Parent theRoot = FXMLLoader.load(getClass().getResource("InstructionalWindow.fxml"));
-		theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		theScene = new Scene(theRoot);
-		theStage.setScene(theScene);
-		theStage.show();
+    void SwitchToInstructionalWindow(ActionEvent event) throws IOException, SQLException {
+    	if(HelpSystem.userDatabaseHelper.checkAuth(HelpSystem.getUsername()).contains("insctrutor")) {
+	    	Parent theRoot = FXMLLoader.load(getClass().getResource("InstructionalWindow.fxml"));
+			theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			theScene = new Scene(theRoot);
+			theStage.setScene(theScene);
+			theStage.show();
+    	}
+    	else {
+    		warningLabel.setVisible(false);
+			explosionLabel.setVisible(true);
+			explosion.setVisible(true);
+			
+			pause.setOnFinished(e-> explosion.setVisible(false));
+			pause2.setOnFinished(e-> explosionLabel.setVisible(false));
+			pause3.setOnFinished(e-> warningLabel.setVisible(true));
+			
+			pause.play();
+			pause2.play();
+			pause3.play();
+    	}
     }
 
     @FXML
-    void SwitchToStudentWindow(ActionEvent event) throws IOException {
-    	Parent theRoot = FXMLLoader.load(getClass().getResource("StudentWindow.fxml"));
-		theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		theScene = new Scene(theRoot);
-		theStage.setScene(theScene);
-		theStage.show();
+    void SwitchToStudentWindow(ActionEvent event) throws IOException, SQLException {
+    	if(HelpSystem.userDatabaseHelper.checkAuth(HelpSystem.getUsername()).contains("student")) {
+	    	Parent theRoot = FXMLLoader.load(getClass().getResource("StudentWindow.fxml"));
+			theStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			theScene = new Scene(theRoot);
+			theStage.setScene(theScene);
+			theStage.show();
+    	}
+    	else {
+    		warningLabel.setVisible(false);
+			explosionLabel.setVisible(true);
+			explosion.setVisible(true);
+			
+			pause.setOnFinished(e-> explosion.setVisible(false));
+			pause2.setOnFinished(e-> explosionLabel.setVisible(false));
+			pause3.setOnFinished(e-> warningLabel.setVisible(true));
+			
+			pause.play();
+			pause2.play();
+			pause3.play();
+    	}
     }
 }
